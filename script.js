b9619e13edff8876e2c8bf11b9f59031a126b03d
hanssenstijn <hanssenstijn@gmail.com>
2020-05-05 12:33:50 +0200

    for the api key

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // whatever is in the dotenv will be loaded into environment
 require('dotenv').config();
 // check if the variable API_KEY value is distributed
-console.log(process.env);
-
+console.log(process.env); 
 // locally example npm install dotenv
 const api_key = process.env.API_KEY;
 console.log(api_key);
